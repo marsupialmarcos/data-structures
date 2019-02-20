eef4ed599e2b1ee631784109c9829f332c3b212b
Marcos Da Silva <dasilva.marcos@gmail.com>
2019-02-20 15:29:36 -0800

    initial commit

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,31 @@
 var Stack = function() {
-  var someInstance = {};
+  
+  var someInstance = {}; //create object
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0 //keeps current count of elements in object, does not currespond to a key-value pair until pushed.
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  
+  someInstance.push = function(value) { 
+    storage[count] = value;
+    count++
   };
 
   someInstance.pop = function() {
+    if (count === 0){
+        return undefined
+    } count-- //decrease count by one to be able to access value to delete, otherwise would exceed elements in object by one.
+    var poppedVal = storage[count] // holds popped value to return
+    delete storage[count]
+
+    return poppedVal
   };
 
-  someInstance.size = function() {
-  };
+  someInstance.size = function(){
+      return count
+  }
 
   return someInstance;
 };
