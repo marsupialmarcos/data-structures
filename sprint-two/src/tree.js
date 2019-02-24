c72214941d595118d555ca2e92bb21a2e7ffc981
Marcos Da Silva <dasilva.marcos@gmail.com>
2019-02-23 16:08:34 -0800

    added tree struct

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,18 +2,35 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods)
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+    var current = this.children
+    current.push(Tree(value))
 };
 
 treeMethods.contains = function(target) {
+  var find = function(treeNode){
+    var found = false;
+    if (treeNode.value === target){
+      return true
+    } else {
+      var node = treeNode.children
+      for (var i = 0; i < node.length; i++){
+        found = find(node[i])
+        if (found === true){
+          return found
+        }
+      }
+      return found
+    }
+  }
+  return find(this)
 };
 
 
